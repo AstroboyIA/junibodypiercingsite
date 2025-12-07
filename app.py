7b0fba895f1c26ccdf0b19f0868b5c4eb59af4ba
Walisson Paulo da Silva <walisson200939@gmail.com>
2025-12-07 03:05:42 -0300

    Deus me Ajuda

--- app.py
+++ app.py
@@ -207,8 +207,8 @@ def excluir_produto(id):
 
     return redirect(url_for('admin'))
 
-
-if __name__ == '__main__':
-    with app.app_context():
+with app.app_context():
         db.create_all()
     #app.run(debug=os.getenv('DEBUG', 'False') == 'True')#
+
+if __name__ == '__main__':
