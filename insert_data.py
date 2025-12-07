e2f017292e59e91e4d1a13da6d4426bae0fb34eb
Walisson Paulo da Silva <walisson200939@gmail.com>
2025-12-07 18:15:46 -0300

    Ajustes

--- insert_data.py
+++ insert_data.py
@@ -1,24 +1,20 @@
-# insert_data.py
-
 from app import db, Produto, app
 
-# 1. Coloca a aplicação em contexto
 app.app_context().push()
 
-# 2. **ATENÇÃO:** Garante que o DB seja recriado (apague o test.db antes de rodar este script)
-db.drop_all()  # Opção de segurança para dropar as tabelas antigas
+
+db.drop_all()
 db.create_all()
 
 print("Tabelas recriadas com sucesso.")
 
-# 3. CRIA OS OBJETOS DE PRODUTO COM A BARRA CORRETA
 p1 = Produto(
     nome='Piercing Argola Ouro 18K',
     descricao='Joia maciça em ouro 18K.',
     preco=350.00,
     estoque=5,
     tipo='18K Gold',
-    imagem_url='images/orelha-ouro.jpg'  # <-- Caminho correto
+    imagem_url='images/orelha-ouro.jpg'
 )
 p2 = Produto(
     nome='Labret Titânio Zircônia',
@@ -26,13 +22,11 @@ p2 = Produto(
     preco=120.00,
     estoque=12,
     tipo='ASTM F-136',
-    imagem_url='images/orelha-prata.jpg' # <-- Caminho correto
+    imagem_url='images/orelha-prata.jpg'
 )
 
-# 4. Adiciona e Salva no banco de dados
 db.session.add_all([p1, p2])
 db.session.commit()
 
-# 5. Confirmação
 print("Produtos inseridos com sucesso!")
 print(Produto.query.all())
